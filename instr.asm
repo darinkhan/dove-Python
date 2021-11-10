0887d27968bff2478cc816c539b8a89a935eaafc
Darin Khan <darinkhan@darins-m1-mbp.acasmart.jh.edu>
2021-11-09 20:39:39 -0500

    Fixed crashing errors and added comments to python_converter

---
+++ instr.asm
@@ -0,0 +1,14 @@
+def $1 [1:2] [1:4)]
+
+		dataset name
+
+end $1
+
+x
+x
+def $2 [1:2] [1:4)]
+
+	 	+ $1 $None
+
+end $2
+
--- python_converter.py
+++ python_converter.py
@@ -5,10 +5,10 @@ s = inp.split(' ')
 v1 = s[0]
 name = s[2].split('"')[1]
 row = s[3][:-1]
-col = s[4][:-1]
+col = s[4][:-1] #need to fix accidental addition of ')' to instr.asm here, maybe .split('\s | ( | ) | , | =')?
 
 d = {v1:1}
-boo = False
+boo = False #what's this for?
 
 orig_stdout = sys.stdout
 f = open('instr.asm', 'w')
@@ -16,7 +16,7 @@ sys.stdout = f
 
 i = 1
 print("def ${} [1:{}] [1:{}]\n".format(i, row, col))
-print("dataset {}\n".format(name))
+print("\t\tdataset {}\n".format(name))
 print("end ${}\n".format(i))
 i += 1
 
@@ -31,9 +31,11 @@ for line in sys.stdin:
     else:
         s = line.split(' ')
         d[s[0]] = i
-    
+        #print(s[2].equals(s[4]))
+        print(s[2])
+        print(s[4])
         print("def ${} [1:{}] [1:{}]\n".format(i, row, col))
-        print("{} ${} ${}\n".format(s[3], d[s[2]], d[s[4]])) #Should throw an error if var doesn't exist in dictionary
+        print("\t \t{} ${} ${}\n".format(s[3], d.get(s[2]), d.get(s[3]))) #Should throw an error if var doesn't exist in dictionary
         print("end ${}\n".format(i))
         
     i += 1
